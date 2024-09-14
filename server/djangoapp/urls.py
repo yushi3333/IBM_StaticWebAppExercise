6406b193ffc71572eb7a02413290daa95109bf2e
yushi3333 <a1125189291@gmail.com>
2024-09-13 23:53:54 -0400

    third update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,9 +7,9 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='register', view=views.registration, name='register'),
     # path for login
-    path(route='login/', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
     path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -47,8 +47,34 @@ def logout_request(request):
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
-# def registration(request):
-# ...
+def registration(request):
+    context = {}
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    email_exist = False
+    try:
+        #check if the user already exists
+        User.objects.get(username=username)
+        username_exist = True
+    except:
+        logger.debug("{} is new user".format(username))
+    
+    if not username_exist:
+        user = User.objects.create_user(username=username, first_name = first_name, last_name=last_name,password=password, email=email)
+        login(request, user)
+        data={"userName":username, "status": "Authenticated"}
+        return JsonResponse(data)
+    else:
+        data = {"userName":username, "error": "Already Registered"}
+        return JsonResponse(data)
+
+
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
